9481d712461500d50ae401fecb473e286ca2411f
juliaDz03 <juliadziekanska2003@gmail.com>
2024-01-17 09:57:34 +0100

    add project files

--- src/main/java/tu/kielce/airnexcontrolsystem/services/TicketService.java
+++ src/main/java/tu/kielce/airnexcontrolsystem/services/TicketService.java
@@ -8,4 +8,30 @@ import tu.kielce.airnexcontrolsystem.commends.BuyTicketCommand;
 public interface TicketService {
     void refundTicket(Long id);
     void buyTicket(BuyTicketCommand command);
+
+    /**
+     * @author Julia Dziekańska
+     */
+    public List<TicketDto> getAllTicketsForUserById(Long userId, boolean onlyActive) {
+        List<Ticket> tickets;
+
+        if (onlyActive) {
+            tickets = ticketRepository.findByUserIdAndActiveTrue(userId);
+        } else {
+            tickets = ticketRepository.findByUserId(userId);
+        }
+
+        return tickets.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+
+        private TicketDto convertToDto(Ticket ticket) {
+            TicketDto ticketDto = new TicketDto();
+
+            ticketDto.setId(ticket.getId());
+            ticketDto.setTitle(ticket.getTitle());
+
+            return ticketDto;
+        }
+    }
 }
